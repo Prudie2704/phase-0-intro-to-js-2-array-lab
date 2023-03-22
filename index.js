1ca487541cddb2f15022784c366690f06f0e51b2
prudence <prudence.jerono@student.moringaschool.com>
2023-03-22 23:09:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+function cats(){
+    console.log("cats:", Milo, Otis, Garfield);
+}
+const cats = ["Milo", "Otis", "Garfield"]
+cats.length;
+ // => 3
+
+ const cats = ["Milo", "Otis", "Gardfield"]
+ cats.push("Ralph")
+ // => 4
+
+ cats;
+ // => ["Milo", "Otis", "Garfield", "Ralph"]
+
+ const cats = ["Milo", "Otis", "Gardfield"]
+ cats.pop();
+ // => "Ralph"
+
+ cats;
+ // => ["Milo", "Otis", "Garfield"]
+
+ const cats = ["Milo", "Otis", "Gardfield"]
+ cats.pop();
+ // => "Garfield"
+
+ cats;
+ // => ["Milo", "Otis"]
+
+ const cats = ["Milo", "Otis", "Gardfield"]
+ cats.shift();
+ // => ["Milo"]
+
+ cats;
+ // => ["Otis", "Gardfield"]
+
+const cats = ["Milo", "Otis", "Gardfield"]
+   console.log('cats:', Milo, Otis, Gardfield);
+    
